f99f82625a235c72ec7e00c8e4913cea244db188
Ronak-dataanalysis <ronak.fathi91@gmail.com>
2023-12-09 11:54:34 +0330

    converting c to f and vice versa

---
+++ celsuis & fahrenheit.py
@@ -0,0 +1,8 @@
+#celsuis to fahrenheit
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit")
+#fahrenheit to celsius
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+celsius = (fahrenheit -32)*5/9
+print(f"{fahrenheit} degree Fahrenheit is equal to {celsius} degree Celsius")
