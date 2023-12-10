c0fd3e3eb3bcae046502c5b73740fcc174afe910
Ronak-dataanalysis <ronak.fathi91@gmail.com>
2023-12-10 11:19:31 +0330

    sum of natural numbers with limitation

---
+++ sum of natural numbers.py
@@ -0,0 +1,6 @@
+"""calculating sum of natural numbers"""
+limit = int(input("Enter the limit: "))
+thesum = 0
+for i in range(1, limit + 1):
+ thesum += i
+print("The sum of natural numbers up to", limit, "is:", thesum)
