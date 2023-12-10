b59372f18f4a5cb9b885b92dc339f47d1d29e8c5
Ronak-dataanalysis <ronak.fathi91@gmail.com>
2023-12-10 12:32:54 +0330

    update

--- aCalculator.py
+++ aCalculator.py
@@ -1,12 +1,16 @@
 """make a calculator with simple operations"""
 def add(x, y):
- return x + y
+    """adding"""
+    return x + y
 def subtract(x, y):
- return x - y
+    """minus"""
+    return x - y
 def multiply(x, y):
- return x * y
+    """multiplying"""
+    return x * y
 def divide(x, y):
- return x / y
+    """dividing"""
+    return x / y
 
 print("Select operation.")
 print("1.Add")
@@ -17,11 +21,11 @@ while True:
     choice = input("Enter choice(1/2/3/4): ")
     if choice in ('1', '2', '3', '4'):
         try:
-           num1 = float(input("Enter first number: "))
-           num2 = float(input("Enter second number: "))
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
         except ValueError:
-           print("Invalid input. Please enter a number.")
-           continue
+            print("Invalid input. Please enter a number.")
+            continue
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
         elif choice == '2':
@@ -32,6 +36,6 @@ while True:
             print(num1, "/", num2, "=", divide(num1, num2))
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
-           break
+            break
     else:
         print("Invalid Input")
