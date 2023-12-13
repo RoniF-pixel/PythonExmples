7143f8ee9859a9048cc4f5df5a4eb1e08ff9729f
Ronak-dataanalysis <ronak.fathi91@gmail.com>
2023-12-13 12:19:31 +0330

    The N largest

--- lists.py
+++ lists.py
@@ -17,3 +17,17 @@ if len(numbers) >= 2:
     print("The second largest number in the list is:", second_largest)
 else:
     print("The list does not contain a second largest number.")
+
+#N largest
+def find_n_largest_elements(lst, n):
+ # Sort the list in descending order
+ sorted_lst = sorted(lst, reverse=True)
+ # Get the first N elements
+ largest_elements = sorted_lst[:n]
+ return largest_elements
+numbers = [70, 10, 35, 5, 20, 50, 15, 3, 345, 54, 67, 87, 98]
+# Number of largest elements to find
+N = int(input("N =  " ))
+# Find the N largest elements from the list
+result = find_n_largest_elements(numbers, N)
+print(f"The {N} largest elements in the list are:", result)
