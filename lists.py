911a8c723ca8c8f85bc5eceef818851566e1bf02
Ronak-dataanalysis <ronak.fathi91@gmail.com>
2023-12-13 12:07:43 +0330

    second largest

--- lists.py
+++ lists.py
@@ -6,3 +6,14 @@ for i in numbers:
     if i > minimum:
         minimum = i
 print("The largest number in the list is:", minimum)
+
+#second largest
+numbers = [55, 27, -97, 12, 2]
+# Sort the list in descending order
+numbers.sort(reverse=True)
+# Check if there are at least two elements in the list
+if len(numbers) >= 2:
+    second_largest = numbers[1]
+    print("The second largest number in the list is:", second_largest)
+else:
+    print("The list does not contain a second largest number.")
