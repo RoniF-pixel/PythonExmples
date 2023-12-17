bae731eb06f697e7d3720d7fd501291cc8f87a20
Ronak-dataanalysis <ronak.fathi91@gmail.com>
2023-12-17 17:41:00 +0330

    update remove ith character of a string

--- removeCharacter.py
+++ removeCharacter.py
@@ -1,13 +1,13 @@
 """Remove a character from a string"""
-def remove_char(inputStr, i):
-    if i < 0 or i >= len(inputStr):
+def remove_char(input_Str, i):
+    if i < 0 or i >= len(input_Str):
         print(f"Invalid index {i}. The string remains unchanged.")
-        return inputStr
- # Remove the i-th character using slicing
-    resultStr = inputStr[:i] + inputStr[i + 1:]
-    return resultStr
-inputStr = "Hello! let's start it."
-i = 7 
-newString = remove_char(inputStr, i)
-print(f"Original String: {inputStr}")
-print(f"String after removing {i}th character : {newString}")
+        return input_Str
+ #Remove the i-th character using slicing
+    result_Str = input_Str[:i] + input_Str[i + 1:]
+    return result_Str
+sentence = "Hello! elet's start it."
+j = 7 
+new_String = remove_char(sentence, j)
+print(f"Original String: {sentence}")
+print(f"String after removing {j}th character : {new_String}")
