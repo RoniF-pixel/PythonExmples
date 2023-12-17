ae8281bdbb6dc4e0327003a966214f4574687a44
Ronak-dataanalysis <ronak.fathi91@gmail.com>
2023-12-17 17:05:21 +0330

    getting words with length of greater than "H"

---
+++ LenghtGreater.py
@@ -0,0 +1,15 @@
+""" A program to identify words with length of greater than L """
+def find_words(words, H):
+    #Create an empty list to store words greater than H
+    result = []
+
+    for i in words:
+    #Check if the length of the i is greater than H
+        if len(i) > H:
+    #If yes, append it to the result list
+            result.append(i)
+    return result
+word_list = ["clue", "christmas", "news", "introduction", "newspaper", "fueles"]
+H = 6
+long_words = find_words(word_list, H)
+print(f"Words longer than {H} characters: {long_words}")
